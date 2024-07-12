6eb72d34f27da0cd0a7cff1f275b9156af825933
harshdeep61034 <hdeep61034@gmail.com>
2024-07-12 22:19:38 +0530

    Simple primitives

---
+++ simplevalues/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// integer
+	fmt.Println(69)
+
+	// string
+	fmt.Println("The Boys")
+
+	// floats
+	fmt.Println(7.3)
+
+	// bool
+	fmt.Println(true)
+}
