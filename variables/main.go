c4819ecf1e7bfd58c3a9bb3279132b5666322427
harshdeep61034 <hdeep61034@gmail.com>
2024-07-13 20:31:41 +0530

    Variables in go

---
+++ variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// mannual
+	var name string = "Harsh"
+
+	// infer
+	var isadult = true
+
+	var age int = 18
+
+	// used when you have to decalre and assign at the same time
+	fact := "he is a coder"
+
+	fmt.Println("Hello, " + name)
+	fmt.Println(isadult)
+	fmt.Println(age)
+	fmt.Println(fact)
+}
